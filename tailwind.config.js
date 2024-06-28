202d88dc7cb6b9f717c6ce4802a0d855ee1954cc
natsuto6 <79001972+natsuto6@users.noreply.github.com>
2024-06-28 18:07:02 +0900

    chore: slim-railsのインストール、application.html.erbをapplication.html.slimに変換、tailwind.config.jsの設定を編集

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./app/views/**/*.html.slim",
+    "./app/helpers/**/*.rb",
+    "./app/frontend/**/*.js",
+    "./app/frontend/**/*.jsx",
+    "./app/frontend/**/*.ts",
+    "./app/frontend/**/*.tsx",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
