ce15048c120e5c6ef0dc6cd65c3164c9f4237f92
natsuto6 <79001972+natsuto6@users.noreply.github.com>
2024-07-16 17:40:25 +0900

    chore: rubocopの指摘箇所を修正

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :game do
     user { nil }
     remaining_time { 1 }
-    score { "9.99" }
+    score { '9.99' }
     correct_count { 1 }
     wrong_count { 1 }
   end
