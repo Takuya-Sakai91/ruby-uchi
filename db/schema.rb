54bf3db18160a3dc354f7d1f24e66ecfd1badcac
natsuto6 <79001972+natsuto6@users.noreply.github.com>
2024-07-16 17:34:51 +0900

    feat: Gameモデルとテーブルの作成、認証と関連付け

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_28_150133) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_16_074705) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "remaining_time"
+    t.decimal "score"
+    t.integer "correct_count"
+    t.integer "wrong_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_games_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +37,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_28_150133) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "games", "users"
 end
