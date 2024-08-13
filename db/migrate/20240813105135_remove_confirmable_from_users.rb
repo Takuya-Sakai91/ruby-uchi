85cc43054f8c846e7954b78e78ea002a05da6d52
natsuto6 <79001972+natsuto6@users.noreply.github.com>
2024-08-13 20:07:21 +0900

    refactor: deviseのConfirmableモジュールを削除し、ユーザー登録フローを簡素化

---
+++ db/migrate/20240813105135_remove_confirmable_from_users.rb
@@ -0,0 +1,9 @@
+class RemoveConfirmableFromUsers < ActiveRecord::Migration[7.1]
+  def change
+    remove_index :users, :confirmation_token
+    remove_column :users, :confirmation_token, :string
+    remove_column :users, :confirmed_at, :datetime
+    remove_column :users, :confirmation_sent_at, :datetime
+    remove_column :users, :unconfirmed_email, :string
+  end
+end
