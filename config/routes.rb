3b00ad6749849e2b8d830a2150b69c4acf0c245b
natsuto6 <79001972+natsuto6@users.noreply.github.com>
2024-08-21 23:50:49 +0900

    chore: bin/lintの指摘箇所を修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
@@ -5,9 +7,9 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
-  resources :games, only: [:new, :create, :show] do
+  resources :games, only: %i[new create show] do
     member do
       post 'finish'
       post 'post'
