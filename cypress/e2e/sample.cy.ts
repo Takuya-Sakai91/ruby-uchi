746427a846f902f25eba1d8745d44e7f3bbd4e9e
natsuto6 <79001972+natsuto6@users.noreply.github.com>
2024-09-09 22:49:03 +0900

    style: Prettierでコードスタイルを修正

--- cypress/e2e/sample.cy.ts
+++ cypress/e2e/sample.cy.ts
@@ -1,6 +1,6 @@
-describe('My First Test', () => {
-  it('Visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('h1', 'Ruby打ち')
-  })
-})
+describe("My First Test", () => {
+  it("Visits the app root url", () => {
+    cy.visit("/");
+    cy.contains("h1", "Ruby打ち");
+  });
+});
