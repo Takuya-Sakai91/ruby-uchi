2de37d1a2ff00da795d62d8e0af9babdd4296472
natsuto6 <79001972+natsuto6@users.noreply.github.com>
2024-09-15 13:51:51 +0900

    feat: RubyMethodモデルを作成

---
+++ app/models/ruby_method.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+class RubyMethod < ApplicationRecord
+  belongs_to :module_ref, class_name: 'Module'
+  has_many :game_methods
+end
---
+++ spec/factories/ruby_methods.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :ruby_method do
+    module_ref { nil }
+    name { "MyString" }
+    description { "MyText" }
+    official_url { "MyString" }
+    class_name { "MyString" }
+  end
+end
