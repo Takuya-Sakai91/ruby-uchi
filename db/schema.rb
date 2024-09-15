45ea9e5be1fcef379f01b4f34535bbcf75d15686
natsuto6 <79001972+natsuto6@users.noreply.github.com>
2024-09-15 23:51:06 +0900

    fix: usernameカラムのデフォルト値とnull: false制約の復元

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_15_073820) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_15_144807) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,7 +60,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_15_073820) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username"
+    t.string "username", default: "", null: false
     t.decimal "best_score"
     t.decimal "previous_score"
     t.index ["email"], name: "index_users_on_email", unique: true
