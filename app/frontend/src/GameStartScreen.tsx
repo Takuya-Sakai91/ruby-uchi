4c0bb3e7286b03d0359846497b7d9bf43b5beb05
natsuto6 <79001972+natsuto6@users.noreply.github.com>
2024-10-04 00:03:53 +0900

    feat: createPortalを使用し、モーダルの実装を修正

--- app/frontend/src/GameStartScreen.tsx
+++ app/frontend/src/GameStartScreen.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import Modal from "./components/Modal";
+import { Modal } from "./components/Modal";
+import { CircleHelp } from "lucide-react";
 
 export default function GameStartScreen() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -22,45 +23,58 @@ export default function GameStartScreen() {
   };
 
   return (
-    <div className="flex justify-center text-center min-h-screen p-4">
-      <div className="flex-col">
+    <div className="flex flex-col justify-center text-center bg-white min-h-screen p-4">
+      <div>
         <h1 className="text-4xl font-bold mb-4">
           SpaceかEnterを押してスタート！
         </h1>
-        <p className="text-lg mb-8">
+        <p className="text-lg mb-4">
           ※ゲーム中は「Esc」キーですぐにタイピングをやり直すことができます。
         </p>
         <button
           className="btn bg-red-600 hover:bg-red-700 text-white w-full mb-4 max-w-xs"
           onClick={() => setIsModalOpen(true)}
         >
+          <CircleHelp className="size-[1rem]" />
           遊び方
         </button>
         <a href="/" className="btn btn-outline btn-primary w-full max-w-xs">
           TOPへ戻る
         </a>
       </div>
-      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
-        <h2 className="text-2xl font-bold mb-4">遊び方</h2>
-        <p>Ruby打ちはRubyメソッドも学べるタイピングゲームです。</p>
-        <p>※制限時間は60秒です。</p>
-        <p>
-          ・画面中央にRubyメソッドが表示されます。画面内にある内にメソッドをタイピングすると次のメソッドが表示されます。
-        </p>
-        <p>
-          ・タイピングには時間制限があります。時間内にたくさんのメソッドを打ち込みましょう！
-        </p>
-        <p>
-          ・ゲーム中は「Esc」キーですぐにタイピングをやり直すことができます。
-        </p>
-        <p>
-          ・ゲーム中は「Shift +
-          D」キーでRubyメソッドの解説を見ることができます。
-        </p>
-        <p>
-          ・さらにメソッドの解説画面で「Shift +
-          D」キーを押すとRubyの公式ドキュメントが別ウィンドウで開きます。
-        </p>
+      <Modal
+        open={isModalOpen}
+        setOpen={setIsModalOpen}
+        className="w-[100%] sm:w-[420px]"
+      >
+        <h3 className="text-center text-3xl font-bold">遊び方</h3>
+        <div className="my-4 text-center">
+          <p>Ruby打ちはRubyメソッドも学べるタイピングゲームです。</p>
+          <p>※制限時間は60秒です。</p>
+          <p>
+            ・画面中央にRubyメソッドが表示されます。画面内にある内にメソッドをタイピングすると次のメソッドが表示されます。
+          </p>
+          <p>
+            ・タイピングには時間制限があります。時間内にたくさんのメソッドを打ち込みましょう！
+          </p>
+          <p>
+            ・ゲーム中は「Esc」キーですぐにタイピングをやり直すことができます。
+          </p>
+          <p>
+            ・ゲーム中は「Shift +
+            D」キーでRubyメソッドの解説を見ることができます。
+          </p>
+          <p>
+            ・さらにメソッドの解説画面で「Shift +
+            D」キーを押すとRubyの公式ドキュメントが別ウィンドウで開きます。
+          </p>
+          <button
+            className="btn btn-outline w-full max-w-xs"
+            onClick={() => setIsModalOpen(false)}
+          >
+            閉じる
+          </button>
+        </div>
       </Modal>
     </div>
   );
--- app/frontend/src/components/Modal.tsx
+++ app/frontend/src/components/Modal.tsx
@@ -1,25 +1,68 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { createPortal } from "react-dom";
 
-type ModalProps = {
-  isOpen: boolean;
-  onClose: () => void;
-  children: React.ReactNode;
-};
+import clsx, { ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
-export default function Modal({ isOpen, onClose, children }: ModalProps) {
-  if (!isOpen) return null;
+const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
 
-  return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 justify-center items-center p-4 z-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full">
-        {children}
-        <button
-          onClick={onClose}
-          className="btn bg-gray-700 hover:bg-gray-800 text-white mt-4 w-full max-w-xs"
-        >
-          閉じる
-        </button>
-      </div>
-    </div>
-  );
+interface ModalProps {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  className?: string;
+  children?: React.ReactNode;
 }
+
+export const Modal = ({ open, setOpen, className, children }: ModalProps) => {
+  useEffect(() => {
+    if (open) {
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+  }, [open]);
+
+  if (typeof document === "undefined") return null;
+
+  return createPortal(
+    <AnimatePresence>
+      {open && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0, transition: { delay: 0.4 } }}
+          onClick={() => setOpen(false)}
+          className="fixed inset-0 z-50 flex justify-center items-center bg-black bg-opacity-50 p-8 backdrop-blur"
+        >
+          <motion.div
+            initial={{ scale: 1, rotate: "0deg", y: -100 }}
+            animate={{ scale: 1, rotate: "0deg", y: 0 }}
+            exit={{
+              scale: 1,
+              rotate: "0deg",
+              y: -100,
+              transition: { delay: 0.3 },
+            }}
+            onClick={(e) => e.stopPropagation()}
+            className={cn(
+              "w-full relative max-w-lg cursor-default overflow-hidden rounded-lg bg-white p-6 text-slate-700 shadow-[0_6px_30px_-4px_rgba(17,33,95,0.2)]",
+              className
+            )}
+          >
+            <motion.div
+              initial={{ opacity: 0, y: 0 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: 0, transition: { delay: 0 } }}
+              transition={{ type: "linear", duration: 0.2, delay: 0.3 }}
+              className="relative z-10"
+            >
+              {children}
+            </motion.div>
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>,
+    document.body
+  );
+};
