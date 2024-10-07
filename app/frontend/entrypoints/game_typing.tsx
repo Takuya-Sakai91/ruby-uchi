660f5e9993f340e7585c7d27487a7687a0966285
natsuto6 <79001972+natsuto6@users.noreply.github.com>
2024-10-07 23:51:48 +0900

    feat: タイピングゲーム用のエントリーポイントを追加

---
+++ app/frontend/entrypoints/game_typing.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import TypingGame from "../src/TypingGame";
+
+ReactDOM.createRoot(document.getElementById("typing-game")!).render(
+  <React.StrictMode>
+    <TypingGame />
+  </React.StrictMode>,
+)
