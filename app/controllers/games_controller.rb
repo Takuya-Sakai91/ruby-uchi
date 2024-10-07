ad7a31bd5f7011889a2754c3ec912ccc16a90baa
natsuto6 <79001972+natsuto6@users.noreply.github.com>
2024-10-07 23:46:08 +0900

    fix: ゲーム開始時にユーザー認証を不要にし、ゲーム作成ロジックを追加

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,15 +1,29 @@
 # frozen_string_literal: true
 
 class GamesController < ApplicationController
-  before_action :authenticate_user!, only: %i[create show finish post methods]
+  before_action :authenticate_user!, only: %i[show finish post methods]
 
-  def show; end
+  def show
+    @game = Game.find(params[:id])
+  end
 
   def new
     @game = Game.new
   end
 
-  def create; end
+  def create
+    @game = current_user.games.create(
+      remaining_time: 60,
+      score: 0,
+      correct_count: 0,
+      wrong_count: 0
+    )
+    if @game.persisted?
+      redirect_to game_path(@game)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   def finish; end
 
